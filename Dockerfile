e29780756663e655abf6d8673b77eec3b2871b70
localhost <xpaomian@gmail.com>
2025-05-09 17:49:44 +0800

    chore: metrics table file path error (#16)
    * chore: metrics table file path error
    
    * chore: add root command for project
    
    * chore: change tools name

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM golang:1.23 as builder
 
 ENV LANG en_US.utf8
+
 WORKDIR /metrics-loader
+
+# RUN go env -w  GOPROXY=https://goproxy.cn,direct
 
 COPY . .
 RUN make
@@ -10,6 +13,6 @@ FROM ubuntu:22.04 as base
 
 WORKDIR /metrics-loader
 COPY configs /configs
-COPY --from=builder /metrics-loader/bin/sample_loader /usr/local/bin/
+COPY --from=builder /metrics-loader/bin/* /usr/local/bin/
 
 ENTRYPOINT ["sample_loader"]
--- cmd/timeseries_analyzer/main.go
+++ cmd/timeseries_analyzer/main.go
@@ -2,67 +2,13 @@ package main
 
 import (
 	"log"
-	"metrics-bench-suite/pkg/parser"
-	"metrics-bench-suite/pkg/timeseries"
-	"metrics-bench-suite/pkg/utils/decode"
 
-	"github.com/spf13/cobra"
+	"metrics-bench-suite/pkg/cmd/timeseries_analyzer"
 )
 
-// Analyzer is the struct for the timeseries analyzer
-type Analyzer struct{}
-
-// Run is the main function to run the timeseries analyzer
-func (a *Analyzer) Run(cmd *cobra.Command, args []string) error {
-	if len(args) < 1 {
-		log.Fatal("You must provide input data")
-	}
-
-	filePath := args[0]
-	log.Printf("Processing file: %s", filePath)
-
-	tableNameCounter := make(map[string]bool)
-	timeSeriesCounter := make(timeseries.Counter)
-
-	requests, err := parser.ParseHTTPRequests(filePath)
-	if err != nil {
-		log.Printf("Error parsing request file %s: %v", filePath, err)
-		return err
-	}
-
-	for _, request := range requests {
-		wr, err := decode.Body(request.Body)
-		if err != nil {
-			log.Fatalf("failed to decode body: %v", err)
-		}
-		err = timeseries.CountTableName(&wr, &tableNameCounter)
-		if err != nil {
-			log.Fatalf("failed to count table names: %v", err)
-		}
-		err = timeseries.CountAllTimeSeries(&wr, &timeSeriesCounter)
-		if err != nil {
-			log.Fatalf("failed to count time series: %v", err)
-		}
-	}
-
-	log.Printf("Found %d table names", len(tableNameCounter))
-	log.Printf("Found %d time series", len(timeSeriesCounter))
-
-	return nil
-}
-
 func main() {
-	analyzer := &Analyzer{}
 
-	var rootCmd = &cobra.Command{
-		Use:   "timeseries_analyzer <tcpflow_output_dir>",
-		Short: "Timeseries Analyzer is a tool to analyze timeseries in tcpflow output",
-		Run: func(cmd *cobra.Command, args []string) {
-			if err := analyzer.Run(cmd, args); err != nil {
-				log.Fatalf("Error processing tcpflow files: %v", err)
-			}
-		},
-	}
+	var rootCmd = timeseries_analyzer.NewCommand()
 
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalf("Error processing tcpflow files: %v", err)
